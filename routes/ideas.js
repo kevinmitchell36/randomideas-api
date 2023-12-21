b0eab24bf02f100380ce09f83fb485b13f905b68
kevinmitchell36 <kevingm31@gmail.com>
2023-12-21 16:16:47 -0600

    Add mongoose, mongodb, routes, and models
    Mongodb and mongoose integrated. Routes updated for database calls.

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,77 +1,74 @@
 const express = require('express');
 const router = express.Router();
-
-const ideas = [
-  {
-    id: 1,
-    text: 'An app that helps you organize gym routines',
-    tag: 'Lifestyle',
-    username: 'Sophie',
-    date: '2023-12-21'
-  },  
-  {
-    id: 2,
-    text: 'Tour guide app',
-    tag: 'Entertainment',
-    username: 'Alice',
-    date: '2023-12-21'
-  },
-  {
-    id: 3,
-    text: 'Finding local history app',
-    tag: 'Entertainment',
-    username: 'Kevin',
-    date: '2023-12-21'
-  },
-]
-
-router.get('/', (req, res) => {
-  res.json({ data: ideas });
-});
+const Idea = require('../models/Idea')
 
 
-router.get('/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id)
-  if (!idea) {
-    return res.status(404).json({success: false, error: 'Resource not found'})
+router.get('/', async (req, res) => {
+  try {
+    const ideas = await Idea.find();
+    res.json({ success: true, data: ideas })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({success: false, error: 'Something went wrong'})
   }
-  res.json( { data: idea } );
 });
 
-router.post('/', (req, res) => {
-  const idea = {
-    id: ideas.length + 1,
+
+router.get('/:id', async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
+    res.json({ success: true, data: idea })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({success: false, error: 'Something went wrong'});
+  }
+});
+
+
+router.post('/', async (req, res) => {
+  const idea = new Idea ({
     text: req.body.text,
     tag: req.body.tag,
     username: req.body.username,
-    date: new Date().toISOString().slice(0, 10),
-  };
+  });
 
-  ideas.push(idea)
-  res.json({ success: true, data: idea})
+  try {
+    const savedIdea = await idea.save();
+    res.json({ success: true, data: savedIdea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong'})
+  }
 });
 
-router.put('/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id)
-  if (!idea) {
-    return res.status(404).json({success: false, error: 'Resource not found'})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedIdea = await Idea.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          text: req.body.text,
+          tag: req.body.tag
+        }
+      },
+      { new: true}
+    );
+    res.json({ success: true, data: updatedIdea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong'});
   }
-
-  idea.text = req.body.text || idea.text;
-  idea.tag = req.body.tag || idea.tag;
-
-  res.json( { data: idea } );
 });
 
-router.delete('/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id)
-  if (!idea) {
-    return res.status(404).json({success: false, error: 'Resource not found'})
+router.delete('/:id', async (req, res) => {
+  try {
+    await Idea.findByIdAndDelete(req.params.id);
+    res.json({ success: true, data: {} })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong'});
   }
-
-  const index = ideas.indexOf(idea);
-  ideas.splice(index, 1)
-  res.json( { success: true, data: {} } );
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const port = 3000;
 const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 8000;
+const connectDB = require('./config/db');
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+
 
 
 
@@ -11,4 +17,4 @@ const ideasRouter = require('./routes/ideas');
 app.use('/api/ideas', ideasRouter);
 
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(port);

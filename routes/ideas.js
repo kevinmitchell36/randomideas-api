671ff7e07cb1758b06f62bfd5c058ded77516472
kevinmitchell36 <kevingm31@gmail.com>
2023-12-21 12:37:39 -0600

    Update routes to include all requests
    post, put, delete of hard-coded data

--- routes/ideas.js
+++ routes/ideas.js
@@ -26,7 +26,7 @@ const ideas = [
 ]
 
 router.get('/', (req, res) => {
-  res.json({data: ideas});
+  res.json({ data: ideas });
 });
 
 
@@ -35,8 +35,43 @@ router.get('/:id', (req, res) => {
   if (!idea) {
     return res.status(404).json({success: false, error: 'Resource not found'})
   }
-  res.json( {data: idea} );
+  res.json( { data: idea } );
 });
 
+router.post('/', (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  };
+
+  ideas.push(idea)
+  res.json({ success: true, data: idea})
+});
+
+router.put('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id)
+  if (!idea) {
+    return res.status(404).json({success: false, error: 'Resource not found'})
+  }
+
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+
+  res.json( { data: idea } );
+});
+
+router.delete('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id)
+  if (!idea) {
+    return res.status(404).json({success: false, error: 'Resource not found'})
+  }
+
+  const index = ideas.indexOf(idea);
+  ideas.splice(index, 1)
+  res.json( { success: true, data: {} } );
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+
 const ideasRouter = require('./routes/ideas');
-
-
 app.use('/api/ideas', ideasRouter);
 
 

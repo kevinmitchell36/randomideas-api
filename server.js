e9e8005a36abf15d6152272764ecb822840aa21b
kevinmitchell36 <kevingm31@gmail.com>
2024-01-02 12:05:49 -0600

    Add webpack fontend

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const path = require('path');
 const express = require('express');
-const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 8000;
 const connectDB = require('./config/db');
 
 connectDB();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
